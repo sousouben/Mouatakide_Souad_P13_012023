3600bef9ecdf71a8d9b8ff3a4ade956ef356f1bb
SOUSOUBENH <75803460+sousouben@users.noreply.github.com>
2023-01-31 12:09:26 +0100

    modif header

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -2,35 +2,34 @@ import React from "react";
 import "./header.css";
 import logo from "../../assets/img/argentBankLogo.png";
 import { Link, useNavigate } from "react-router-dom";
-import { selectUser } from "../../feature/selector";
-import { useSelector } from "react-redux/es/hooks/useSelector";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { signOut, axiosUserData } from "../../services/actions";
+import { selectUser } from "../../feature/selector";
 
 const Header = () => {
-  const userData = useSelector(selectUser);
-  const token =
-    localStorage.getItem("token") || sessionStorage.getItem("token");
-  const isRemembered = localStorage.getItem("isRemembered");
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  function clearStorage() {
+  const userData = useSelector(selectUser);
+  const isRemembered = localStorage.getItem("isRemembered");
+
+  const handleSignOut = () => {
     localStorage.clear();
     sessionStorage.clear();
-    return dispatch(signOut());
-  }
+    dispatch(signOut());
+  };
 
-  function remember() {
+  const handleSignIn = () => {
     if (isRemembered) {
+      const token =
+        localStorage.getItem("token") || sessionStorage.getItem("token");
       dispatch(axiosUserData(token));
       navigate("/profile");
     } else {
       navigate("/login");
     }
-  }
+  };
 
-  return token && userData.data ? (
+  return (
     <nav className="main-nav">
       <Link to="/" className="main-nav-logo">
         <img
@@ -40,32 +39,15 @@ const Header = () => {
         />
         <h1 className="sr-only">Argent Bank</h1>
       </Link>
-      <div>
-        <Link to="/profile" className="main-nav-item">
-          <i className="fa fa-user-circle"></i>
-          {userData.data.firstName}
-        </Link>
-        <Link to="/" onClick={clearStorage} className="main-nav-item">
-          <i className="fa fa-sign-out"></i>
-          Sign Out
-        </Link>
-      </div>
-    </nav>
-  ) : (
-    <nav className="main-nav">
-      <Link to="/" className="main-nav-logo">
-        <img
-          className="main-nav-logo-image"
-          src={logo}
-          alt="Argent Bank Logo"
-        />
-        <h1 className="sr-only">Argent Bank</h1>
-      </Link>
-      <div onClick={remember} className="main-nav-item">
+      <div
+        onClick={userData.data ? handleSignOut : handleSignIn}
+        className="main-nav-item"
+      >
         <i className="fa fa-user-circle"></i>
-        Sign In
+        {userData.data ? `${userData.data.firstName} Sign Out` : `Sign In`}
       </div>
     </nav>
   );
 };
+
 export default Header;
